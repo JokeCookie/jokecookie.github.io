2766bb2080205e9bd9e654c2f2530d469b04e881
jokecookie <127332160+JokeCookie@users.noreply.github.com>
2023-04-29 20:36:56 +0200

    Update dummheitstest.js

--- dummheitstest.js
+++ dummheitstest.js
@@ -7,7 +7,7 @@ const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'Ich wusste es ;)';
+    question.innerHTML = 'Wilkommen im Club!';
 });
 
 noBtn.addEventListener('mouseover', () => {
